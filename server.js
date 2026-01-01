d87e955aa411ac893c712812b1befc52d43edb6a
skuffazzer-ai <dadada1111111111111@gmail.com>
2026-01-01 21:05:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,43 +5,36 @@ const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
 
-// Раздаём статические файлы из папки public
 app.use(express.static('public'));
 
-// Хранение ожидания одного пользователя для соединения
 let waitingUser = null;
 
 io.on('connection', socket => {
-  console.log('Новый пользователь подключился:', socket.id);
+    console.log('Новый пользователь:', socket.id);
 
-  if (waitingUser) {
-    // Если уже есть кто-то в ожидании, соединяем их
-    socket.emit('partner-found', { partnerId: waitingUser.id });
-    waitingUser.emit('partner-found', { partnerId: socket.id });
-    waitingUser = null;
-  } else {
-    // Иначе ставим пользователя в очередь
-    waitingUser = socket;
-    socket.emit('waiting', 'Ждём собеседника...');
-  }
+    if (waitingUser) {
+        // соединяем с ожидающим
+        socket.emit('partner-found', { partnerId: waitingUser.id });
+        waitingUser.emit('partner-found', { partnerId: socket.id });
+        waitingUser = null;
+    } else {
+        waitingUser = socket;
+        socket.emit('waiting', 'Ждем собеседника...');
+    }
 
-  // Обработка сигналов WebRTC
-  socket.on('signal', data => {
-    io.to(data.partnerId).emit('signal', { signal: data.signal, from: socket.id });
-  });
+    socket.on('signal', data => {
+        io.to(data.partnerId).emit('signal', { signal: data.signal, from: socket.id });
+    });
 
-  // Обработка сообщений чата
-  socket.on('chat-message', data => {
-    io.to(data.partnerId).emit('chat-message', { message: data.message, from: socket.id });
-  });
+    socket.on('chat-message', data => {
+        io.to(data.partnerId).emit('chat-message', { message: data.message, from: socket.id });
+    });
 
-  // Когда пользователь отключается
-  socket.on('disconnect', () => {
-    if (waitingUser && waitingUser.id === socket.id) waitingUser = null;
-    socket.broadcast.emit('partner-disconnected', socket.id);
-    console.log('Пользователь отключился:', socket.id);
-  });
+    socket.on('disconnect', () => {
+        if (waitingUser && waitingUser.id === socket.id) waitingUser = null;
+        socket.broadcast.emit('partner-disconnected', socket.id);
+        console.log('Пользователь отключился:', socket.id);
+    });
 });
 
-// Запуск сервера
 http.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
