eb80d6fbed16a44fd64bf85b3077c5e2e379f247
skuffazzer-ai <dadada1111111111111@gmail.com>
2026-01-01 23:36:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,30 +6,39 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
-
 let waitingUser = null;
 
 io.on("connection", socket => {
   console.log("User connected:", socket.id);
 
+  // Когда пользователь готов искать собеседника
   socket.on("readyForPeer", () => {
-    if(waitingUser && waitingUser.id !== socket.id) {
+    if (waitingUser && waitingUser.id !== socket.id) {
       const peer1 = waitingUser;
       const peer2 = socket;
+
+      // Оповещаем обоих, что найден собеседник
       peer1.emit("foundPeer", { peerId: peer2.id });
       peer2.emit("foundPeer", { peerId: peer1.id });
+
       waitingUser = null;
     } else {
       waitingUser = socket;
     }
   });
 
-  socket.on("offer", ({ target, sdp }) => io.to(target).emit("offer", { sdp, from: socket.id }));
-  socket.on("answer", ({ target, sdp }) => io.to(target).emit("answer", { sdp }));
-  socket.on("iceCandidate", ({ target, candidate }) => io.to(target).emit("iceCandidate", { candidate }));
+  // Передача WebRTC сигналов (offer/answer/ice)
+  socket.on("signal", data => {
+    io.to(data.peerId).emit("signal", {
+      signal: data.signal,
+      from: socket.id
+    });
+  });
 
-  socket.on("disconnect", () => { if(waitingUser && waitingUser.id === socket.id) waitingUser=null; });
+  socket.on("disconnect", () => {
+    if (waitingUser && waitingUser.id === socket.id) waitingUser = null;
+    console.log("User disconnected:", socket.id);
+  });
 });
 
-server.listen(process.env.PORT || 3000, () => console.log("Server running"));
+server.listen(3000, () => console.log("Server running on port 3000"));
