3d9cb65fc259527f60141f327819eeef9d4fe6da
skuffazzer-ai <dadada1111111111111@gmail.com>
2026-01-02 00:06:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const path = require("path");
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 let waitingUser = null;
 
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+  console.log("Новый пользователь подключен:", socket.id);
 
   socket.on("readyForPeer", () => {
     if (waitingUser && waitingUser.id !== socket.id) {
@@ -26,24 +22,18 @@ io.on("connection", (socket) => {
     }
   });
 
-  socket.on("offer", (data) => {
-    io.to(data.to).emit("offer", { sdp: data.sdp, from: socket.id });
-  });
-
-  socket.on("answer", (data) => {
-    io.to(data.to).emit("answer", { sdp: data.sdp, from: socket.id });
-  });
-
-  socket.on("ice-candidate", (data) => {
-    io.to(data.to).emit("ice-candidate", { candidate: data.candidate, from: socket.id });
+  socket.on("signal", (data) => {
+    io.to(data.peerId).emit("signal", {
+      signal: data.signal,
+      from: socket.id,
+    });
   });
 
   socket.on("disconnect", () => {
     if (waitingUser && waitingUser.id === socket.id) waitingUser = null;
-    io.emit("peerDisconnected", { id: socket.id });
-    console.log("User disconnected:", socket.id);
+    console.log("Пользователь отключился:", socket.id);
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+http.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
