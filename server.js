f08842531028ae4b2fe8775e9c409a044bc5c905
skuffazzer-ai <dadada1111111111111@gmail.com>
2026-01-01 22:40:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,33 +8,40 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-let waiting = null;
+let waitingUser = null;
 
-io.on("connection", socket => {
-  if (waiting) {
-    socket.partner = waiting;
-    waiting.partner = socket;
+io.on("connection", (socket) => {
+  socket.on("ready", () => {
+    if (waitingUser) {
+      socket.partner = waitingUser;
+      waitingUser.partner = socket;
 
-    socket.emit("match", { initiator: true });
-    waiting.emit("match", { initiator: false });
+      socket.emit("match");
+      waitingUser.emit("match");
 
-    waiting = null;
-  } else {
-    waiting = socket;
-  }
-
-  socket.on("signal", data => {
-    if (socket.partner) {
-      socket.partner.emit("signal", data);
+      waitingUser = null;
+    } else {
+      waitingUser = socket;
     }
   });
 
+  socket.on("offer", (data) => {
+    socket.partner?.emit("offer", data);
+  });
+
+  socket.on("answer", (data) => {
+    socket.partner?.emit("answer", data);
+  });
+
+  socket.on("ice-candidate", (data) => {
+    socket.partner?.emit("ice-candidate", data);
+  });
+
   socket.on("disconnect", () => {
+    if (waitingUser === socket) waitingUser = null;
     if (socket.partner) {
-      socket.partner.emit("leave");
       socket.partner.partner = null;
     }
-    if (waiting === socket) waiting = null;
   });
 });
 
