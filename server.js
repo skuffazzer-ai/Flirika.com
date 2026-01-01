0b5d4c89854c010890e761bb83822bf71cc786fd
skuffazzer-ai <dadada1111111111111@gmail.com>
2026-01-01 20:52:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,11 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(express.static("public"));
 
-const server = http.createServer(app);
-const io = new Server(server);
-
-let waitingUser = null;
-
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
-
-  socket.on("join", () => {
-    if (waitingUser) {
-      const partner = waitingUser;
-      waitingUser = null;
-
-      socket.partner = partner.id;
-      partner.partner = socket.id;
-
-      socket.emit("partnerFound", partner.id);
-      partner.emit("partnerFound", socket.id);
-    } else {
-      waitingUser = socket;
-      socket.emit("waiting");
-    }
-  });
-
-  socket.on("signal", (data) => {
-    if (socket.partner) {
-      io.to(socket.partner).emit("signal", { from: socket.id, data });
-    }
-  });
-
-  socket.on("disconnect", () => {
-    if (waitingUser && waitingUser.id === socket.id) waitingUser = null;
-    if (socket.partner) io.to(socket.partner).emit("partnerDisconnected");
-  });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-server.listen(process.env.PORT || 3000, () => console.log("Server running..."));
+app.listen(port, () => console.log(`Server running on port ${port}`));
